80926303babf724e974cd9b68df8946e7b3a91a4
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-29 22:13:08 +0200

    Initialising Amazon Clone Project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="w-full bg-amazon_blue text-white px-4 py-3">
+            
+        </div>
+    )
+}
+
+export default Header
