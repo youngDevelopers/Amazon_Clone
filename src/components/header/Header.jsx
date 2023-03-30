8398c9dad6fe970df1b76541520f47623d91985e
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-30 09:58:03 +0200

    Setting up Vercel

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import SearchIcon from '@mui/icons-material/Search';
 import {logo} from "../../assets/assetsExports";
+import categories from '../../constants/index';
 
 function Header() {
+
+    const [showAll, setShowAll] = useState(false);
+    
+    
+
     return (
         <div>
             <div className="w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-4">
@@ -18,14 +24,33 @@ function Header() {
                 </div>
 
                 <div className="h-10 rounded-md flex flex-grow relative" >
-                    <span className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm text-amazon_blue font-titleFont  flex items-center justify-center rounded-t1-md rounded-b1-md " >
+                    <span onClick={() => setShowAll(!showAll)} className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm text-amazon_blue font-titleFont  flex items-center justify-center rounded-t1-md rounded-b1-md " >
                         All <span><ArrowDropDownIcon /></span>
                     </span>
+                    {
+                        showAll && (
+                            <div>
+                                <ul className="absolute w-56 h-80 top-10  left-0 overflow-y-scroll overflow-x-hidden bg-white border-[1px] border-amazon_blue text-black p-2 flex-col  gap-1 z-50" >
+                                    {
+                                        categories && categories.map( (item)=> {
+                                            return(
+                                                <li key={item._id} className="text-sm tracking-wide font-titleFont border-b-[1px] border-b-transparent hover:border-b-amazon_blue cursor-pointer duration-200" >
+                                                    {item.title}
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        )
+                    }
                     <input className="h-full text-base text-amazon_blue flex-grow outline-none border-none" type="text"/>
-                    <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md "  >
+                    <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md ">
                         <SearchIcon />
                     </span>
                 </div>
+
+
                 
             </div>
         </div>
---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+export const categories= [
+    {
+        _id:"100",title:"All Departments"
+    },
+    {
+        _id:"101",title:"Automotive"
+    },
+    {
+        _id:"102",title:"Baby"
+    },
+    {
+        _id:"103",title:"Beauty & Personal Care"
+    },-
+    {
+        _id:"104",title:"Boys' Fashion"
+    },
+    {
+        _id:"105",title:"Schools"
+    },
+    {
+        _id:"106",title:"Electronics"
+    },
+    {
+        _id:"107",title:"JOJO Tanks"
+    },
+    {
+        _id:"108",title:"Cars"
+    },
+    {
+        _id:"109",title:"Shipments"
+    },
+    {
+        _id:"110",title:"Accomodation"
+    },
+    {
+        _id:"111",title:"Hardware"
+    },
+    {
+        _id:"112",title:"Groceries"
+    },
+    {
+        _id:"113",title:"Jobs"
+    },
+    {
+        _id:"114",title:"News"
+    },
+    {
+        _id:"115",title:"Softwares"
+    },
+]
