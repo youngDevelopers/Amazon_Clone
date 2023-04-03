4d708853f5b25cfba2b1d16d5dbb5a22c942e706
youngDevelopers <dambaemmanuel5@gmail.com>
2023-04-03 12:11:14 +0200

    Adding component structure to the Side Nav Bar Content 2

--- src/components/header/SideBarNavContent.jsx
+++ src/components/header/SideBarNavContent.jsx
@@ -5,7 +5,7 @@ function SideBarNavContent({title,one,two,three}) {
     return (
         <div className="py-3 border-b-[1px] border-b-gray-300" >
             <h3 className="text-lg font-titleFont font-semibold mb-1 px-6" >{title}</h3>
-            <ul>
+            <ul className="text-sm" >
                 <li className="flex items-center justify-betweem hover:bg-zinc-200 px-6 py-2 cursor-pointer" >{one}<span><KeyboardArrowRightIcon /></span> </li>
                 <li className="flex items-center justify-betweem hover:bg-zinc-200 px-6 py-2 cursor-pointer" >{two}<span><KeyboardArrowRightIcon /></span> </li>
                 <li className="flex items-center justify-betweem hover:bg-zinc-200 px-6 py-2 cursor-pointer" >{three} <span><KeyboardArrowRightIcon /></span> </li>
