11c03be0340ef79c1d13f0b284657ecfaf6fc77f
youngDevelopers <dambaemmanuel5@gmail.com>
2023-04-06 16:43:56 +0200

    Footer Top

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Banner from './components/home/Banner';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="font-bodyFont" >
-        <h1>
           <Header />
           <Banner />
-        </h1>
+          <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterTop from './FooterTop'
+
+function Footer() {
+    return (
+        <div className="font-titleFont" >
+            <FooterTop />
+        </div>
+    )
+}
+
+export default Footer
