c0835ef7c09a02e73a254c981c2d3390257ea029
youngDevelopers <dambaemmanuel5@gmail.com>
2023-04-07 12:22:38 +0200

    Footer Edited

--- src/components/footer/FooterMiddle.jsx
+++ src/components/footer/FooterMiddle.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import FooterMiddleList from './FooterMiddleList'
+import React from 'react';
+import FooterMiddleList from './FooterMiddleList';
+import middleList from '../../constants/index';
 
 function FooterMiddle() {
     return (
@@ -7,10 +8,11 @@ function FooterMiddle() {
             <div className="w-full border-b-[1px] border-gray-500 py-10">
                 <div className="max-w-5xl mx-auto text-gray-500" >
                     <div className="w-full grid grid-cols-4" >
-                        <FooterMiddleList />
-                        <FooterMiddleList />
-                        <FooterMiddleList />
-                        <FooterMiddleList />
+                        {
+                            middleList.map( (item) =>{
+                                <FooterMiddleList key={item._id} title={item.title} listItem={item.listItem}/>
+                            })
+                        }
                     </div>
                 </div>
             </div>
--- src/components/footer/FooterMiddleList.jsx
+++ src/components/footer/FooterMiddleList.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-function FooterMiddleList() {
+function FooterMiddleList({title,listItem}) {
     return (
         <div>
-            <h3 className="font-titleFont text-white text-base font-semibold mb-3">Get to know us</h3>
+            <h3 className="font-titleFont text-white text-base font-semibold mb-3">{title}</h3>
             <ul className="flex flex-col gap-2 font-bodyFont">
-                <li className="footerLink">Careers</li>
-                <li className="footerLink">Careers</li>
-                <li className="footerLink">Careers</li>
-                <li className="footerLink">Careers</li>
+                {
+                    listItem.map( (item) =>{
+                        item.listData.map( (data,i) =>{
+                            <li key={i} className="footerLink">{data}</li>
+                        })
+                    })
+                }
             </ul>
         </div>
     )
