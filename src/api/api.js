3a8a83f196ac08a39ad675981790e07e741ddf21
youngDevelopers <dambaemmanuel5@gmail.com>
2023-04-08 13:04:45 +0200

    Corrected end point api 1

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export async function productsData(){
     const products = await axios.get(
-         "https://jsonplaceholder.typicode.com/photos"
+        "https://dummyjson.com/products"
+         //"https://jsonplaceholder.typicode.com/photos"
         //"https://fakestoreapiserver.vercel.app/amazonproducts"
     );
     return products
